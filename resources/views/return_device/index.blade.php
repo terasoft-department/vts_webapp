ac89ba25d914edb0611d4558e61320ded7e14b1c
wills <archiectwills@gmail.com>
2024-10-30 14:27:29 +0300

    Add: last changes 30/10

--- resources/views/return_device/index.blade.php
+++ resources/views/return_device/index.blade.php
@@ -276,8 +276,8 @@
                                                     <div class="mb-3">
                                                         <label for="status" class="form-label">Status</label>
                                                         <select class="form-select" id="status" name="status" required>
-                                                            <option value="approved" {{ $return->status == 'approved' ? 'selected' : '' }}>Approved</option>
-                                                            <option value="rejected" {{ $return->status == 'rejected' ? 'selected' : '' }}>Rejected</option>
+                                                            <option value="approved">Approved</option>
+                                                            <option value="rejected">Rejected</option>
                                                         </select>
                                                     </div>
                                                     <div class="modal-footer">
