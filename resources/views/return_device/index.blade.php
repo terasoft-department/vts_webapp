201ad8a7b84ca16584d90b0671ffd674fdb9f67c
wills <archiectwills@gmail.com>
2024-10-30 15:08:44 +0300

    Add: last changes 2024 tyr

--- resources/views/return_device/index.blade.php
+++ resources/views/return_device/index.blade.php
@@ -271,7 +271,7 @@
                                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                             </div>
                                             <div class="modal-body">
-                                                <form action="{{ route('return_device.updateStatus', $return->return_id) }}" method="POST">
+                                                <form action="{{ route('return_device.updateStatus', $return->return_id) }}" method="PUT">
                                                     @csrf
                                                     @method('PUT')
                                                     <div class="mb-3">
