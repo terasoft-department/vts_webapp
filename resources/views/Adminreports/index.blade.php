852f15f9a007c4dc3d2bd26a4cf0de6f73c31450
wills <archiectwills@gmail.com>
2024-11-16 00:52:39 +0300

    save invoice mpya

--- resources/views/Adminreports/index.blade.php
+++ resources/views/Adminreports/index.blade.php
@@ -150,7 +150,7 @@
                     <tr><td>{{ $report->id }}</td>
                         <td>{{ $report->reported_date }}</td>
                         {{-- <td>{{ optional($customers->find($report->customer_id))->customername ?? 'Unknown' }}</td>--}}
-                        <td>{{ $report->customername }}</td>
+                        <td>{{ $report->customer->customername ?? 'N/A' }}</td>
                         <td>{{ $report->bus_plate_number }}</td>
                         <td>{{ $report->contact }}</td>
                         <td>{{ $report->reported_by }}</td>
