8f30dccbacbf08942d62d7282bfe071eaa6aadeb
wills <archiectwills@gmail.com>
2024-11-18 01:48:09 +0300

    LEO USIKU NI MOTO

--- resources/views/sdevices/index.blade.php
+++ resources/views/sdevices/index.blade.php
@@ -28,7 +28,8 @@
 
   <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 </head>
 <body>
   <!-- ======= Header ======= -->
