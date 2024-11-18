c193b1a2dadbcf5f3e53a572474aa6c72eb97c8d
wills <archiectwills@gmail.com>
2024-11-18 09:02:07 +0300

    LEO USIKU NI MOTO

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -11,28 +11,28 @@ class CustomerController extends Controller
 {
     public function index()
     {
-        Route::get('/get-customer-details', function (Request $request) {
-        $plateNumber = $request->query('plate_number');
+    //     Route::get('/get-customer-details', function (Request $request) {
+    //     $plateNumber = $request->query('plate_number');
 
-        // Find customer based on the plate number
-        $customer = Customer::where('plate_number', $plateNumber)->first();
+    //     // Find customer based on the plate number
+    //     $customer = Customer::where('plate_number', $plateNumber)->first();
 
-        if ($customer) {
-            return response()->json([
-                'success' => true,
-                'customer' => [
-                    'id' => $customer->id,
-                    'phone' => $customer->phone,
-                    'debt' => $customer->debt
-                ]
-            ]);
-        } else {
-            return response()->json([
-                'success' => false,
-                'message' => 'Customer not found'
-            ]);
-        }
-    });
+    //     if ($customer) {
+    //         return response()->json([
+    //             'success' => true,
+    //             'customer' => [
+    //                 'id' => $customer->id,
+    //                 'phone' => $customer->phone,
+    //                 'debt' => $customer->debt
+    //             ]
+    //         ]);
+    //     } else {
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Customer not found'
+    //         ]);
+    //     }
+    // });
         // Fetch customers with pagination (e.g., 10 customers per page)
         $customers = Customer::paginate(10);
         $CustomersCount = Customer::count();
@@ -63,27 +63,27 @@ class CustomerController extends Controller
         $customer->update($request->all());
         return redirect()->route('customers.index')->with('success', 'Customer updated successfully.');
     }
-    public function getCustomerDetails(Request $request)
-    {
-        $plateNumber = $request->query('plate_number');
-        $vehicle = Vehicle::where('plate_number', $plateNumber)->first();
+    // public function getCustomerDetails(Request $request)
+    // {
+    //     $plateNumber = $request->query('plate_number');
+    //     $vehicle = Vehicle::where('plate_number', $plateNumber)->first();
 
-        if ($vehicle && $vehicle->customer) {
-            return response()->json([
-                'success' => true,
-                'customer' => [
-                    'id' => $vehicle->customer->customer_id,
-                    'phone' => $vehicle->customer->customer_phone,
-                    // 'debt' => $vehicle->customer->debt,
-                ],
-            ]);
-        } else {
-            return response()->json([
-                'success' => false,
-                'message' => 'Customer details not found for this plate number',
-            ]);
-        }
-    }
+    //     if ($vehicle && $vehicle->customer) {
+    //         return response()->json([
+    //             'success' => true,
+    //             'customer' => [
+    //                 'id' => $vehicle->customer->customer_id,
+    //                 'phone' => $vehicle->customer->customer_phone,
+    //                 // 'debt' => $vehicle->customer->debt,
+    //             ],
+    //         ]);
+    //     } else {
+    //         return response()->json([
+    //             'success' => false,
+    //             'message' => 'Customer details not found for this plate number',
+    //         ]);
+    //     }
+    // }
 
 
 
