50a57a3dd2cdc38f566fe8e8aae0fa931ac3db28
wills <archiectwills@gmail.com>
2024-11-18 10:58:21 +0300

    LEO USIKU NI MOTO

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -12,7 +12,7 @@ class CustomerController extends Controller
     {
         // Fetch customers with pagination (e.g., 10 customers per page)
         $customers = Customer::all();
-        $customers = Customer::paginate(10);
+        $customers = Customer::paginate(1000);
         $CustomersCount = Customer::count();
         $VehiclesCount = Vehicle::count();
 
