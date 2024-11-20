31e61867f622039e03fec024ab63542ecc393959
wills <archiectwills@gmail.com>
2024-11-20 07:02:07 +0300

    save invoice mpya

--- database/migrations/2024_09_27_104649_create_assignments_table.php
+++ database/migrations/2024_09_27_104649_create_assignments_table.php
@@ -24,7 +24,9 @@ return new class extends Migration
             $table->string('attachment')->nullable(); // Nullable attachment field
             $table->string('assigned_by')->nullable();
             $table->string('status')->nullable();
+            $table->timestamp('created_at')->nullable(); 
             $table->string('accepted_at')->nullable();
+
             $table->timestamps();
         });
     }
