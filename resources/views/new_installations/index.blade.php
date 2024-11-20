875086f88811b827e2a5fe811f1639c73d628003
wills <archiectwills@gmail.com>
2024-11-20 21:21:30 +0300

    save invoice mpya

--- resources/views/new_installations/index.blade.php
+++ resources/views/new_installations/index.blade.php
@@ -258,14 +258,6 @@
             </div>
         @endif
 
-        <!-- Search Bar -->
-        {{-- <form action="{{ route('new_installations.index') }}" method="GET" class="mb-3">
-            <div class="input-group">
-                <input type="text" name="search" class="form-control" placeholder="Search by Customer Name" value="{{ request('search') }}">
-                <button type="submit" class="btn btn-primary">Search</button>
-            </div>
-        </form> --}}
-
         <!-- DataTable with export buttons -->
         <table id="installationsTable" class="table table-bordered">
             <thead>
@@ -373,7 +365,31 @@
         $('#installationsTable').DataTable({
             dom: 'Bfrtip',
             buttons: [
-                'copy', 'excel', 'pdf', 'print' // Enable Copy, Excel, PDF, and Print options
+                {
+                    extend: 'copy',
+                    text: 'Copy',
+                    className: 'btn btn-outline-primary btn-sm'
+                },
+                {
+                    extend: 'excelHtml5',
+                    text: 'Excel',
+                    className: 'btn btn-outline-success btn-sm',
+                    title: 'Installations List'
+                },
+                {
+                    extend: 'pdfHtml5',
+                    text: 'PDF',
+                    className: 'btn btn-outline-danger btn-sm',
+                    title: 'Installations List',
+                    orientation: 'landscape',
+                    pageSize: 'A4'
+                },
+                {
+                    extend: 'print',
+                    text: 'Print',
+                    className: 'btn btn-outline-info btn-sm',
+                    title: 'Installations List'
+                }
             ]
         });
     });
