65a1fce131a99aed13cb15aec9e2ee7ae9021f11
wills <archiectwills@gmail.com>
2024-11-21 12:32:47 +0300

    save invoice mpya2

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -23,7 +23,7 @@ class AssignmentController extends Controller
         $assignments = Assignment::with(['customer', 'user', 'vehicle']) // Eager load relationships
             ->when($search, function ($query) use ($search) {
                 // Apply search filter on 'case_reported', 'location', and 'customer_phone'
-                $query->where('customers', 'like', "%{$search}%")
+                $query->where('customer', 'like', "%{$search}%")
                       ->orWhere('location', 'like', "%{$search}%")
                       ->orWhere('customer_phone', 'like', "%{$search}%");
             });
