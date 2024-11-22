971fe0056d2868042b1d37354bd7d4300bc39cc4
wills <archiectwills@gmail.com>
2024-11-22 10:23:24 +0300

    save invoice mpya4

--- resources/views/cdebts/index.blade.php
+++ resources/views/cdebts/index.blade.php
@@ -344,7 +344,7 @@
 <!-- Template Main JS File -->
 <script src="assets/js/main.js"></script>
 
-{{--
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
@@ -369,7 +369,7 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
         <i class="bi bi-arrow-up-short"></i>
-    </a> --}}
+    </a>
     </body>
 
     </html>
