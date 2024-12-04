084a10712e9ccc908ddbeb58db79de8ac4ee8815
wills <archiectwills@gmail.com>
2024-12-04 14:11:55 +0300

    save loginpage 2024

--- resources/views/monitoring_officer/index.blade.php
+++ resources/views/monitoring_officer/index.blade.php
@@ -308,11 +308,6 @@
   </script>
 
 
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/quill/quill.js"></script>
-  <script src="assets/js/main.js"></script>
-
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
